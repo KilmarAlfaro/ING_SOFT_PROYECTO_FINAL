2386c7bddf1e8e1ef7e0dafcea4d019b5f837d1d
KilmarAlfaro <kilmaralfaro72@gmail.com>
2025-09-26 11:55:11 -0700

    avance perfil doctor

---
+++ Proyecto_soft/resources/views/perfilDoc.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Perfil</title>
+    <link rel="stylesheet" href="{{ asset('css/perfildoc.css') }}">
+</head>
+<body>
+
+    
+    <h1 class="titu">DATOS PERSONALES</h1>
+
+    <div class="container">Nombre: <br><br> Apellido: <br><br>Teléfono: 
+</div>
+</body>
+</html>
